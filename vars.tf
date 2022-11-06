00bcda9ce33ea68a731f1fb8a4d48adc525ebbbf
vishalvijayakumarv <vishalvijaykumarv@gmail.com>
2022-11-06 17:31:07 +0530

    first_commit

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-01d08089481510ba2"
+    us-east-2 = "ami-0066d036f9777ec38"
+    us-west-1 = "ami-064562725417500be"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
